c681d29b79e40212a8fdbd74a42edfb28df1bbc1
Flo <florian.schildwaechter@gmail.com>
2023-09-24 22:18:08 +0200

    chore: cleanup comment and unused code

--- src/components/JsonReferenceType.tsx
+++ src/components/JsonReferenceType.tsx
@@ -1,19 +1,10 @@
 import { Component } from "solid-js";
 import { JsonArray } from "./JsonArray";
 import { JsonObject } from "./JsonObject";
-import { indentation } from "./JsonExplorer";
-import { JsonKey } from "./JsonKey";
 import { JsonElementProps, JsonProperty, JsonValue } from "../types";
 
 export const JsonReferenceType: Component<JsonElementProps> = (props) => {
-  //TODO calculate currentIndentation and currentJsonProperty here and pass the key component down
-  const currentIndentation = () =>
-    `${indentation.repeat(props.property.path.length)}`;
-
-  const currentJsonProperty: JsonProperty = {
-    path: props.property.path,
-    value: JSON.stringify(props.property.value),
-  };
+  //TODO potentially calculate currentIndentation and currentJsonProperty here and pass the key component down
 
   const isArrayPropery = (
     property: JsonProperty
